d47cc77de74b3ff742bfe90dff5559d6f1783883
alla5149 <jmakaaa@gmail.com>
2022-10-27 17:01:49 +0300

    Add all files

---
+++ src/main/java/com/zhmaka/Main.java
@@ -0,0 +1,28 @@
+package com.zhmaka;
+
+public class Main {
+    // Homework 1 Task 3
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            if (i == 6) {
+                break;
+            }
+            System.out.println("Крок " + i);
+        }
+        //Task 2
+        int y = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Крок " + i + " значення " + y);
+            y = y + 2;
+
+        }
+        //Task 1
+        String onl3 = "Alla Zhmaka";
+        System.out.println(onl3);
+        // or simple code
+        System.out.println("Alla Zhmaka");
+    }
+}
