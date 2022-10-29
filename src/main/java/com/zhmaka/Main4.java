9b6300159d62309f7479dca06c1661593aa312bf
alla5149 <jmakaaa@gmail.com>
2022-10-29 20:37:32 +0300

    Add first task to HW4

--- src/main/java/com/zhmaka/Main4.java
+++ src/main/java/com/zhmaka/Main4.java
@@ -1,4 +1,21 @@
 package com.zhmaka;
 
 public class Main4 {
+    public static void main(String[] args) {
+        String word = "Hello World";
+        char a = word.charAt(0);
+        char b = word.charAt(10);
+        System.out.println(a + " " + b);
+
+
+        /*String word = "Hello World!";
+        System.out.println("First element in the line is: " + word.substring(word.length()));
+        System.out.println("The last element in the line is: " + word.substring(word.length()));
+        */
+
+
+
+
+      
+    }
 }
