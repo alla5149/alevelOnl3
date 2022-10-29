c9eabccd586d240aae4b1d9ba49e6a3d5da5f436
alla5149 <jmakaaa@gmail.com>
2022-10-29 22:01:16 +0300

    Add second task to HW4

--- src/main/java/com/zhmaka/Main4.java
+++ src/main/java/com/zhmaka/Main4.java
@@ -2,20 +2,22 @@ package com.zhmaka;
 
 public class Main4 {
     public static void main(String[] args) {
+       // 1 Task
         String word = "Hello World";
         char a = word.charAt(0);
         char b = word.charAt(10);
         System.out.println(a + " " + b);
 
+     /*String word = "Hello World!";
+     System.out.println("First element in the line is: " + word.substring(word.length()));
+     System.out.println("The last element in the line is: " + word.substring(word.length()));
+       */
+        // 2 Task 
+        String java = "Java Exercises";
+        String java1 = "Java Exercise";
+        System.out.println(java.equalsIgnoreCase(java1));
 
-        /*String word = "Hello World!";
-        System.out.println("First element in the line is: " + word.substring(word.length()));
-        System.out.println("The last element in the line is: " + word.substring(word.length()));
-        */
 
 
-
-
-      
     }
 }
