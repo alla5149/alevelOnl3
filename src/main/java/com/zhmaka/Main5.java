13d391ca3523cb94ed1ee75818aea3fb91ea9516
alla5149 <jmakaaa@gmail.com>
2022-11-04 00:02:11 +0200

    1 Task

--- src/main/java/com/zhmaka/Main5.java
+++ src/main/java/com/zhmaka/Main5.java
@@ -1,5 +1,18 @@
 package com.zhmaka;
 
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main5 {
-    
+    public static void main(String[] args) {
+        int[] array1 = new int[12];
+        System.out.println(Arrays.toString(array1));
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = ((int) (Math.random() * 31) - 15);
+            System.out.println(Arrays.toString(array1));
+
+            }
+        }
 }
