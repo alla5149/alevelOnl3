8a412551db03769cb35613d8da88df3f4583ce9d
alla5149 <jmakaaa@gmail.com>
2022-11-04 22:37:27 +0200

    To do task 1 again

--- src/main/java/com/zhmaka/Main5.java
+++ src/main/java/com/zhmaka/Main5.java
@@ -1,21 +1,34 @@
 package com.zhmaka;
 
-import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Main5 {
     public static void main(String[] args) {
+        randomArray();
 
     }
-    public 
+// Task 1
+    public static void randomArray() {
         int[] array1 = new int[12];
-        System.out.println(Arrays.toString(array1));
         for (int i = 0; i < array1.length; i++) {
             array1[i] = ((int) (Math.random() * 31) - 15);
             System.out.println(Arrays.toString(array1));
-
+        }
+        System.out.println();
+        int maxNumber = -16;
+        int index = 0;
+        for (int i = 0; i < array1.length; i++) {
+            if (maxNumber <= array1[i]) {
+                maxNumber = array1[i];
+                index = i;
             }
         }
+        System.out.println(index);
+    }
+    //Task 2
+
+
+    
+
+
 }
