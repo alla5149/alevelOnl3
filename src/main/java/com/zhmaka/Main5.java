274ad46d519a01c7538b211975ec9ddcc8aab33d
alla5149 <jmakaaa@gmail.com>
2022-11-04 21:15:03 +0200

    Second task

--- src/main/java/com/zhmaka/Main5.java
+++ src/main/java/com/zhmaka/Main5.java
@@ -7,6 +7,9 @@ import java.util.Random;
 
 public class Main5 {
     public static void main(String[] args) {
+
+    }
+    public 
         int[] array1 = new int[12];
         System.out.println(Arrays.toString(array1));
         for (int i = 0; i < array1.length; i++) {
