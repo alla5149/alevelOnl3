89ce02330fba35d8c58e7355ef395de9f1813560
alla5149 <jmakaaa@gmail.com>
2022-11-04 22:57:56 +0200

    Task 3

--- src/main/java/com/zhmaka/Main5.java
+++ src/main/java/com/zhmaka/Main5.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class Main5 {
     public static void main(String[] args) {
         randomArray();
+        oddNumber();
+        increasingSequence();
 
     }
-// Task 1
+    //Task 1
     public static void randomArray() {
         int[] array1 = new int[12];
         for (int i = 0; i < array1.length; i++) {
@@ -23,12 +25,38 @@ public class Main5 {
                 index = i;
             }
         }
-        System.out.println(index);
+        System.out.println("The last index of the biggest number is " + index);
+
+        System.out.println("This is the start of task 2");
     }
-    //Task 2
+    //Task
+    public static void oddNumber(){
+        int [] array2 = new int[8];
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = ((int) (Math.random() * 9) + 1);
+            System.out.println(Arrays.toString(array2));
+        }
+        System.out.println();
 
-
+        for (int i = 0; i < array2.length; i++) {
+            if (!(i % 2 == 0)) {
+                array2[i] = 0;
+            }
+            System.out.println(Arrays.toString(array2));
+        }
+        System.out.println("This is the start of task 3");
+    }
+public static void increasingSequence(){
+    int [] array2 = new int[8];
+    for (int i = 0; i < array2.length; i++) {
+        array2[i] = ((int) (Math.random() * 89) + 10);
+        System.out.println(Arrays.toString(array2));
+    }
+    System.out.println();
     
+
+}
 
 
 }
+
